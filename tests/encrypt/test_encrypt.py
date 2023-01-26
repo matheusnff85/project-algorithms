bc5e998599bb087f040b544240e88817bb925279
matheusnff85 <math.marinho154@gmail.com>
2023-01-26 12:31:08 -0300

    feat: req 2 test encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("falha", "falha")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(65, 65)
+
+    assert encrypt_message("teste1", 3) == "set_1et"
+    assert encrypt_message("teste2", 3) == "set_2et"
+    assert encrypt_message("teste3", 7) == "3etset"
