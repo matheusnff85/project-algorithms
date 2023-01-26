daf5b40f08f14815f56ff73c257f9d9bf039dc5e
matheusnff85 <math.marinho154@gmail.com>
2023-01-26 12:48:39 -0300

    feat: req 6 palindromes iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,4 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word or len(word) == 0:
+        return False
+    return word == word[::-1]
