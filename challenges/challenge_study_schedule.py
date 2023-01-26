05c6a15281d36227bd5e1f83a93ab9a65779f6cf
matheusnff85 <math.marinho154@gmail.com>
2023-01-26 12:25:36 -0300

    feat: requisito 1 study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    if target_time is None:
+        return None
+    for period in permanence_period:
+        if type(period[0]) != int or type(period[1]) != int:
+            return None
+        if period[0] <= target_time and period[1] >= target_time:
+            counter += 1
+
+    return counter
